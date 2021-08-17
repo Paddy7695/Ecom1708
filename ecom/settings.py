ddb2cec4982e816475417fe426c6a492c83378a5
pradip <pradipchavanchaemail@example.com>
2021-08-18 01:16:46 +0530

    deployements setting 1

--- ecom/settings.py
+++ ecom/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'eshop',
     'rest_framework',
     'whitenoise.runserver_nostatic',
-    'django.contrib.staticfiles',
+
 
 ]
 
@@ -124,14 +124,26 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+MEDIA_URL = '/media/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+
+    os.path.join(BASE_DIR, '../media/static/'),
+
+    os.path.join(BASE_DIR, '/media/'),
+
+]
+
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = "/media/"
+STATIC_ROOT ='/media/static'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 
